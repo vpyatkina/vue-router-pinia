3b724d9501a7b484dec91b20b0ba20f8f4aa4ea4
vpyatkina <vlad.pyatkin@laprairie.com>
2025-03-05 10:05:51 -0700

    feat: custom uploader for files in the form, and dark mode support

--- src/components/LicenseForm.vue
+++ src/components/LicenseForm.vue
@@ -1,8 +1,5 @@
 <script setup>
 import { ref } from 'vue'
-
-import FloatLabel from 'primevue/floatlabel'
-//import InputMask from 'primevue/inputmask'
 import {
   InputNumber,
   InputText,
@@ -12,12 +9,16 @@ import {
   InputGroup,
   InputGroupAddon,
   FileUpload,
+  Button,
+  FloatLabel,
 } from 'primevue'
 import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 
+// Tailwind reactive breakpoints that can be used by Vue
 const breakpoints = useBreakpoints(breakpointsTailwind)
 const mdAndSmaller = ref(breakpoints.smallerOrEqual('md'))
 
+//Object to hold data from form
 const formData = ref({
   licenseNo: null,
   licenseClass: null,
@@ -33,10 +34,11 @@ const formData = ref({
   height: null,
   weight: null,
   issued: null,
+  files: [],
 })
 
+//Arrays holding lists for select fields
 const sexs = ref(['M', 'F', 'X'])
-
 const provinces = ref([
   { name: 'Alberta', code: 'AB' },
   { name: 'British Columbia', code: 'BC' },
@@ -52,11 +54,30 @@ const provinces = ref([
   { name: 'Saskatchewan', code: 'SK' },
   { name: 'Yukon', code: 'YT' },
 ])
+
+//Function to save uploaded files to formData
+function onAdvancedUpload(event) {
+  console.log(event)
+  const files = event.files
+  formData.value.files.push(files)
+  console.log(formData.value.files[0])
+}
+
+function formSubmission(event) {
+  event.preventDefault()
+  console.log(event)
+}
 </script>
 
 <template>
   <div class="flex align-middle justify-center">
-    <form action="" class="bg-white p-10 rounded-4xl mt-5 md:mt-20 w-11/12 md:w-auto">
+    <!-- Big ol' form to collect license data -->
+    <form
+      action=""
+      class="bg-white dark:bg-neutral-800 p-10 rounded-4xl mt-5 md:mt-20 w-11/12 md:w-auto"
+      id="license-form"
+      v-on:submit="formSubmission($event)"
+    >
       <div class="grid grid-cols-1 md:grid-cols-2 gap-5 mb-15">
         <FloatLabel variant="in">
           <InputMask
@@ -64,6 +85,7 @@ const provinces = ref([
             v-model="formData.licenseNo"
             mask="999999-999"
             fluid
+            required
           ></InputMask>
           <label for="license-no">License Number</label>
         </FloatLabel>
@@ -73,34 +95,42 @@ const provinces = ref([
             v-model="formData.licenseClass"
             fluid
             :useGrouping="false"
+            required
           ></InputNumber>
           <label for="license-class">License Class</label>
         </FloatLabel>
 
         <FloatLabel variant="in">
-          <InputMask id="expiry" v-model="formData.expiry" mask="99 aaa 9999" fluid></InputMask>
+          <InputMask
+            id="expiry"
+            v-model="formData.expiry"
+            mask="99 aaa 9999"
+            fluid
+            required
+          ></InputMask>
           <label for="expiry">Expiry</label>
         </FloatLabel>
+        <!-- Div to take up a grid space, does not render when grid is collapsed to 1 collum -->
         <div v-if="mdAndSmaller === false"></div>
 
         <FloatLabel variant="in">
-          <InputText id="last" v-model="formData.lastName" fluid></InputText>
+          <InputText id="last" v-model="formData.lastName" fluid required></InputText>
           <label for="last">Last Name</label>
         </FloatLabel>
         <FloatLabel variant="in">
-          <InputText id="first" v-model="formData.firstName" fluid></InputText>
+          <InputText id="first" v-model="formData.firstName" fluid required></InputText>
           <label for="first">First Name</label>
         </FloatLabel>
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-5 mb-15">
         <FloatLabel variant="in" class="md:col-span-3">
-          <InputText id="address" v-model="formData.address" fluid></InputText>
+          <InputText id="address" v-model="formData.address" fluid required></InputText>
           <label for="address">Street Address</label>
         </FloatLabel>
 
         <FloatLabel variant="in">
-          <InputText id="city" v-model="formData.city" fluid></InputText>
+          <InputText id="city" v-model="formData.city" fluid required></InputText>
           <label for="city">City</label>
         </FloatLabel>
         <FloatLabel variant="in">
@@ -110,6 +140,7 @@ const provinces = ref([
             :options="provinces"
             optionLabel="name"
             fluid
+            required
           ></Select>
           <label for="province">Province</label>
         </FloatLabel>
@@ -119,6 +150,7 @@ const provinces = ref([
             v-model="formData.postalCode"
             mask="*** ***"
             fluid
+            required
           ></InputMask>
           <label for="postal-code">Postal Code</label>
         </FloatLabel>
@@ -126,19 +158,26 @@ const provinces = ref([
 
       <div class="grid grid-cols-1 md:grid-cols-3 gap-5 mb-10">
         <FloatLabel variant="in">
-          <DatePicker id="date-of-birth" v-model="formData.dateOfBirth" fluid />
+          <DatePicker id="date-of-birth" v-model="formData.dateOfBirth" fluid required />
           <label for="date-of-birth">Date of Birth</label>
         </FloatLabel>
+        <!-- Div to take up a grid space, does not render when grid is collapsed to 1 collum -->
         <div v-if="mdAndSmaller === false" class="col-span-2"></div>
 
         <FloatLabel variant="in">
-          <Select id="sex" v-model="formData.sex" :options="sexs" fluid></Select>
+          <Select id="sex" v-model="formData.sex" :options="sexs" fluid required></Select>
           <label for="sex">Sex</label>
         </FloatLabel>
 
         <InputGroup>
           <FloatLabel variant="in">
-            <InputNumber id="height" v-model="formData.height" :use-grouping="false" fluid />
+            <InputNumber
+              id="height"
+              v-model="formData.height"
+              :use-grouping="false"
+              fluid
+              required
+            />
             <label for="height">Height</label>
           </FloatLabel>
           <InputGroupAddon>
@@ -148,31 +187,51 @@ const provinces = ref([
 
         <InputGroup>
           <FloatLabel variant="in">
-            <InputNumber id="weight" v-model="formData.weight" :use-grouping="false" fluid />
+            <InputNumber
+              id="weight"
+              v-model="formData.weight"
+              :use-grouping="false"
+              fluid
+              required
+            />
             <label for="weight">Weight</label>
           </FloatLabel>
           <InputGroupAddon>
-            <span>cm</span>
+            <span>kg</span>
           </InputGroupAddon>
         </InputGroup>
 
         <FloatLabel variant="in">
-          <InputMask id="issued" v-model="formData.issued" mask="99 aaa 9999" fluid></InputMask>
+          <InputMask
+            id="issued"
+            v-model="formData.issued"
+            mask="99 aaa 9999"
+            fluid
+            required
+          ></InputMask>
           <label for="issued">Issued</label>
         </FloatLabel>
       </div>
 
       <FileUpload
         name="license-file"
-        url="/api/upload"
-        @upload="onAdvancedUpload($event)"
+        @uploader="onAdvancedUpload($event)"
+        :custom-upload="true"
         :multiple="true"
         :maxFileSize="1000000"
       >
         <template #empty>
-          <span>Drag and drop files to here to upload.</span>
+          <!-- Shows dialog with number of files uploaded -->
+          <span v-if="formData.files.length === 0">Drag and drop files to here to upload.</span>
+          <span v-else
+            >{{ formData.files.length }} file{{
+              formData.files.length >= 2 ? 's have' : ' has'
+            }}
+            been uploaded.</span
+          >
         </template>
       </FileUpload>
+      <Button type="submit" class="mt-5">Submit</Button>
     </form>
   </div>
 </template>
--- src/components/LoginBox.vue
+++ src/components/LoginBox.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
 
+import msLoginLight from '@/assets/ms-login.svg'
+import msLoginDark from '@/assets/ms-login-dark.svg'
+
+//Function to login the user when the login with MS button is pressed
 async function login() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'azure',
@@ -8,13 +13,30 @@ async function login() {
   console.log(data)
   console.log(error)
 }
+
+// Initialize with the current system theme
+const newColorScheme = ref(
+  window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+)
+
+console.log(newColorScheme.value)
+
+// Update when system theme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+  newColorScheme.value = event.matches ? 'dark' : 'light'
+  console.log(newColorScheme.value)
+})
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center">
-    <div class="bg-white p-5 rounded-3xl text-center">
+    <div class="bg-white dark:bg-neutral-800 p-5 rounded-xl text-center">
       <h1 class="text-primary text-4xl">Login</h1>
-      <img class="mt-10 cursor-pointer" @click="login" src="@/assets/ms-login.svg" />
+      <img
+        class="mt-10 cursor-pointer"
+        @click="login"
+        :src="newColorScheme === 'light' ? msLoginLight : msLoginDark"
+      />
     </div>
   </div>
 </template>
